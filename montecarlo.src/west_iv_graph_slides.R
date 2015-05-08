80f545c567afa3813c71eb2e3b29a08fbe1f0ba9
Gray Calhoun <gray@clhn.org>
2015-05-08 13:59:44 -0400

    Write code to draw graphs for slides

---
+++ montecarlo.src/west_iv_graph_slides.R
@@ -0,0 +1,32 @@
+# General code to convert the Monte Carlo simulations into a table.
+# Copyright 2015 Gray Calhoun
+
+args <- commandArgs(trailingOnly = TRUE)
+outputfile <- "montecarlo/west_iv_slides.pdf"
+datafile <- "montecarlo.full/west_iv.csv"
+
+library(lattice)
+lattice.options(default.theme = standard.theme(color = FALSE))
+
+d <- read.csv(datafile)
+d$P <- factor(d$P)
+d$T <- paste("T =", d$T)
+pdf(outputfile, width = 7, height = 3.5)
+dotplot(P ~ Size | T, d, groups = Method, layout = c(2,1),
+        cex = 1.2,
+        auto.key = list(space = "right"),
+        scales = list(y = list(relation = "free")),
+        ## Next part taken from an R-help post
+        prepanel = function(x, y, ...) {
+            ## drop unused levels
+            yy <- y[, drop = TRUE]
+            ## reset y-limits
+            list(ylim = levels(yy),
+                 yat = sort(unique(as.numeric(yy))))
+        },
+        panel = function(x, y, ...) {
+            ## drop unused levels...again...
+            yy <- y[, drop = TRUE]
+            panel.dotplot(x, yy, ...)
+        })
+dev.off()
